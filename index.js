1cf879f8db52befaa05c058f1eff8a82c87f1935
Sipeng He <hsp8412@hotmail.com>
2022-02-06 11:31:00 -0700

    Implement validation

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+const Joi = require("joi");
 const express = require("express");
 const app = express();
+
+
+app.use(express.json());
 
 const products = [
     { id : 1, name: "Macbook Air", price: 1000},
@@ -15,6 +19,29 @@ app.get("/", (req, res)=>{
 app.get("/api/products", (req,res)=>{
     res.send([1,2,3]);
 });
+
+
+app.post("/api/products", (req,res)=>{
+    const schema = Joi.object({
+        name: Joi.string().min(3).required(),
+        price: Joi.number().min(10).max(11).required()
+    })
+
+    const result = schema.validate(req.body);
+    if(result.error){
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    const product = {
+        id: products.length + 1,
+        name: req.body.name,
+        price: req.body.price
+    }
+    products.push(product);
+    res.send(product);
+})
+
 
 app.get("/api/products/:id", (req,res)=>{
     const product = products.find(p=>p.id === parseInt(req.params.id));
